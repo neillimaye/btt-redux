7fda28ede2d900e192893aefa72766b341eb77f5
Neil <neil.limaye108@gmail.com>
2019-06-03 23:54:10 -0500

    working prototype with dummy data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1 class="display-2">Better Temp Tool</h1>
       <p></p>
-      <h1 class="display-4">A tool by Neil to help medical cannabis patients optimize their vaporizer use.</h1>
+      <h1 class="display-4">A prototype tool by Neil to help medical cannabis patients optimize their vaporizer use.</h1>
       <p></p>
       <TempSlider />
     </div>
