126a6b1c1f14ee3b0aaf27a02d21b7efc6a6af67
Neil <neil.limaye108@gmail.com>
2019-09-21 16:47:30 -0500

    Updates

---
+++ precache-manifest.00b0b3563ea8f1f289f306d629ffae44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb17a0fcdc03f31aa391c4930cc4c02",
+    "url": "/thebettertemptool/index.html"
+  },
+  {
+    "revision": "85288f4c5069cad35023",
+    "url": "/thebettertemptool/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "206c086692672f7788ed",
+    "url": "/thebettertemptool/static/js/2.37afc5e1.chunk.js"
+  },
+  {
+    "revision": "85288f4c5069cad35023",
+    "url": "/thebettertemptool/static/js/main.dc03b460.chunk.js"
+  },
+  {
+    "revision": "0a36f4b5526427c51c1d",
+    "url": "/thebettertemptool/static/js/runtime~main.7d408da1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bettertemptool/precache-manifest.bacc781be41b11873ebd20ed18a674b4.js"
+  "/thebettertemptool/precache-manifest.00b0b3563ea8f1f289f306d629ffae44.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bettertemptool/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thebettertemptool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
